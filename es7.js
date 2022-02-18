7c1fa67a674155acf5943ae47206936c2ca3f9e6
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-02-19 01:35:01 +0530

    ES7 features of JS

---
+++ es7.js
@@ -0,0 +1,26 @@
+//ES7
+
+var String_includes;
+//The includes() method returns true if string contains a specified value, otherwise false.
+let text = "Hello world, welcome to the universe.";
+if (text.includes("Hello")) {
+    console.log("yes i am here!!")
+} else {
+    console.log("NO i am not!!")
+}
+
+var Index_operator;
+//exponential operatior is introduced, and the rsult is equivalent to Math.pow().
+console.log(2 ** 10);// Output 1024
+console.log(Math.pow(2, 10)) // Output 1024
+
+var objKey;
+Object.keys()
+var obj = { foo: "bar", baz: 42 };
+console.log(Object.keys(obj))
+
+var object_value;
+var obj = { foo: "bar", baz: 42 };
+console.log(Object.values(obj));
+
+
