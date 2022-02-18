357ede0a33f2795b64a1bbc7c3fa94e2134e481d
Shivangi Ranjan <shivangiec1098@gmail.com>
2022-02-18 23:20:59 +0530

    Hoisting Feature

---
+++ Hoisting.js
@@ -0,0 +1,21 @@
+//Hoisting:Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution.
+
+//Practice Program1
+function codeHoist(){
+    a = 10;
+    let b = 50;
+}
+codeHoist();
+console.log(a); // 10
+console.log(b); // ReferenceError : b is not defined
+
+//program2
+// var code (global)
+console.log(name); // undefined
+var name = 'Mukul Latiyan';
+
+//program3
+// program to display value
+a = 5;
+console.log(a);
+var a; // 5  
